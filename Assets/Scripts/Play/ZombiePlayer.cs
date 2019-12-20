dcc0593cffeff51e05f4680c923a0102f7014d27
Funzl <darnit3@web.de>
2019-12-20 15:00:18 +0100

    implement zombiePlayer

---
+++ Assets/Scripts/Play/ZombiePlayer.cs
@@ -0,0 +1,32 @@
+﻿using Game;
+using Play.ZombieDice;
+using System.Collections.Generic;
+
+public class ZombiePlayer : Player
+{
+    public bool isWinner = false;
+    public bool hasFinishedRound = false;
+    private int brainScoreAmount = 0;
+    public List<ZombieDice> dicePool; // TODO: proper handling through methods instead of public accessibility
+    public List<ZombieDiceSideValue> diceResult; // TODO: proper handling through methods instead of public accessibility
+
+    public ZombiePlayer(string name) : base(name)
+    {
+        
+    }
+
+    public void setDicePool(List<ZombieDice> dicePool)
+    {
+        this.dicePool = dicePool;
+    }
+
+    public void addBrainScore(int amount)
+    {
+        this.brainScoreAmount += amount;
+    }
+
+    public int getBrainScore()
+    {
+        return this.brainScoreAmount;
+    }
+}
