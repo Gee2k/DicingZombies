84fecd722255ebe85ef182252b46cd7900bb7948
Georg <gee2k@users.noreply.github.com>
2020-01-05 21:25:06 +0100

    - added basic GameMenu, - moved gameCreation call for initialisation into menu

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -6,34 +6,28 @@ public class GameManager : MonoBehaviour
 {
     private GameState _gameState;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        SetupGameStates();
-    }
-
     // Update is called once per frame
     void Update()
     {
         Console.WriteLine("[GameManager] inside");
-        if (_gameState == null)
-        {
-            Console.WriteLine("[GameManager] create new Game");
-            SetupGameStates();
-        }
-        _gameState = _gameState.update();
+        _gameState = _gameState?.update();
     }
 
-    private void SetupGameStates()
+    public void SetupGame(RuleBook ruleBook)
     {
-        SetupState setupState = new SetupState();
-        PlayState playState = new PlayState();
-        EndState endState = new EndState();
+        if (_gameState == null)
+        {
+            Console.WriteLine("[GameManager] create new Game of type " + ruleBook.ruleBookTitle);
+            
+            SetupState setupState = new SetupState();    //todo pass RuleBook to setup which is chosen in the menu by the player
+            PlayState playState = new PlayState();
+            EndState endState = new EndState();
 
-        setupState.SetPlayState(playState);
-        playState.SetEndState(endState);
-        endState.setSetupState(setupState);
+            setupState.SetPlayState(playState);
+            playState.SetEndState(endState);
+            endState.setSetupState(setupState);
 
-        _gameState = setupState;
+            _gameState = setupState;
+        }
     }
 }
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Play.ZombieDice;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameManager _gameManager;
+    private void Start()
+    {
+        _gameManager = (GameManager)GameObject.Find("GameManager").GetComponent(typeof(GameManager));
+    }
+
+    public void PlayGame()
+    {
+        _gameManager.SetupGame(new DicingZombiesRuleBook());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
